ab13c56fd460ff4aa9d09465166b121e16c89cd9
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-11-16 13:04:23 +0500

    complete

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useContext} from 'react';
-import {Routes, Route } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Shows from "./Components/Other Pages/Tv Shows/Shows";
 import Movies from "./Components/Other Pages/Movies/Movies";
@@ -13,32 +13,54 @@ import SignUpPage from "./Components/Other Pages/Login & SignUp/SignUp.jsx";
 import { Context } from './Context Api/Context.jsx';
 
 function App() {
-  const { isLoggedIn, setIsLoggedIn } = useContext(Context)
+  const { isLoggedIn, setIsLoggedIn } = useContext(Context);
+  const navigate = useNavigate();
+
+  // Persist login across refresh
+  useEffect(() => {
+    const storedLogin = localStorage.getItem("isLoggedIn");
+    if (storedLogin === "true") {
+      setIsLoggedIn(true);
+      navigate("/home");
+    }
+  }, []);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "true");
+    navigate("/home");
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+    navigate("/login");
+  };
+
+  if (!isLoggedIn) {
+    return (
+      <Routes>
+        <Route path="/login" element={<LoginPage setIsLoggedIn={handleLogin} />} />
+        <Route path="/signup" element={<SignUpPage setIsLoggedIn={handleLogin} />} />
+        <Route path="*" element={<LoginPage setIsLoggedIn={handleLogin} />} />
+      </Routes>
+    );
+  }
 
   return (
-    <div>
-      {!isLoggedIn ? (
-        <Routes>
-          <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/signup" element={<SignUpPage setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="*" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
-        </Routes>
-      ) : (
-        <>
-          <Navbar setIsLoggedIn={setIsLoggedIn} />
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/tvShows" element={<Shows />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/popular" element={<Popular />} />
-            <Route path="/watch/:type/:id" element={<Watch />} />
-            <Route path="/search" element={<SearchResults />} />
-            <Route path="*" element={<Home />} />
-          </Routes>
-          <Footer />
-        </>
-      )}
-    </div>
+    <>
+      <Navbar setIsLoggedIn={handleLogout} />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/tvShows" element={<Shows />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/watch/:type/:id" element={<Watch />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
